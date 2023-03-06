6b5c6b95931417db943248c8a1fc25871a109c6e
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-03-05 20:49:06 -0500

    can write records to db and logout authed user

--- creating-front-end/src/components/Routes/ReadWrite.js
+++ creating-front-end/src/components/Routes/ReadWrite.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../Navbar';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Button, Row, Col } from 'react-bootstrap';
 import { getAuth } from "firebase/auth";
-import { ref, set, push} from "firebase/database"
+import { ref, set, push, onValue, remove } from "firebase/database"
 import { db } from "../../firebase/Firebase"
-
 
 const ReadWrite = () => {
 
@@ -12,28 +11,30 @@ const ReadWrite = () => {
   const [routes, setRoutes] = useState([]);
   const [userId, setUserId] = useState("");
 
-
   const auth = getAuth();
   const user = auth.currentUser;
-  
- 
 
   useEffect(() => {
-      console.log("user ")
-      console.dir(user)
-      if(user !== null){
-          setUserId(user.uid)
-          console.log("user set")
-      } else {
-          console.log("user not set")
-      }
+    setRoutes([]);
+
+    if (user !== null) {
+      setUserId(user.uid)
+      // console.log("user set")
+    } else {
+      // console.log("user not set")
+    }
+
+
+    // read records
+    onValue(ref(db), snapshot => {
+      const data = snapshot.val().users.favoriteRoutes
+        Object.values(data).map(route => {
+            setRoutes(oldArray => [...oldArray, route])
+        })
+    })
   }, []);
 
-  
-  const handleLocationChange = (event) => {
-    setLocation(event.target.value);
-  };
-  
+
   function handleSubmit() {
     const messagesRef = ref(db, `users/${userId}/favoriteRoutes`)
     const newMsgRef = push(messagesRef);
@@ -44,26 +45,58 @@ const ReadWrite = () => {
       timestamp: Date.now(),
       route_id: newId
     });
-
     console.log("record submitted")
   };
 
-return (
-    <Container fluid>
-    <Navbar/>
-    <Container className="d-flex justify-content-center align-items-center h-100">
-    <div className="border p-5 rounded-lg shadow-sm " style={{ marginTop: '20vh',backgroundColor: '#dbd3d3'}}>
-        <h2 className="text-center mb-4">Add to Database</h2>
-        
-        <input
-        onChange={(e) => setLocation(e.target.value)}
-        />
+/* handle remove from firebase (NOT WORKING)
+function handleDelete(id) {
+  const routesRef = ref(db, `users/${userId}/favoriteRoutes`);
 
-          <Button onClick={handleSubmit} className = "mt-3"variant="primary" block>
-            Add
-          </Button>
-      </div>
-    </Container>
+  setRoutes([])
+  remove(routesRef).then(() => {
+    console.log("location removed");
+  });
+} */
+
+  return (
+    <Container fluid>
+
+
+      <Navbar />
+      <Container className="d-flex justify-content-center align-items-center h-100">
+        <Row>
+          <Col>
+            <div className="border p-5 rounded-lg shadow-sm " style={{ marginTop: '20vh', backgroundColor: '#dbd3d3' }}>
+              <h2 className="text-center mb-4">Add to Database</h2>
+
+              <input
+                onChange={(e) => setLocation(e.target.value)}
+              />
+
+              <Button onClick={handleSubmit} className="mt-3" variant="primary" block>
+                Add
+              </Button>
+
+            </div>
+          </Col>
+          <Col>
+            <div className="border p-5 rounded-lg shadow-sm " style={{ marginTop: '20vh', backgroundColor: '#dbd3d3' }}>
+              <h2 className="text-center mb-4">Values from DB:</h2>
+            
+              {routes.map((val) => (
+                <div className="routes">
+                  {/*onClick={() => handleDelete(val.route_id)} for the below H2 tag*/}
+                  <h2 className="route" >{val.route}</h2>
+                </div>
+              ))}
+
+
+            </div>
+          </Col>
+        </Row>
+
+      </Container>
+
     </Container>
   );
 };
