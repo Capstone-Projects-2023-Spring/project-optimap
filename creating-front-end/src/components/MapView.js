eaf09f264470062780454a8048ff47ab8c8293aa
Dat Nguyen <tuj81499@temple.edu>
2023-03-17 14:36:40 -0400

    Merge Search into Mapview

--- creating-front-end/src/components/MapView.js
+++ creating-front-end/src/components/MapView.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 import Navbar from './Navbar';
 
-const MapView = (props) => {
-  const mapStyles = {
-    width: '100%',
-    height: '100%'
-  };
-
-  return (
-    <div>
-    <Navbar/>
-      <Map
-      google={props.google}
-      zoom={14}
-      style={mapStyles}
-      initialCenter={{ lat: 37.7749, lng: -122.4194 }}
-    />
-    </div>
-  );
+const mapStyles = {////
+  width: '100%',
+  height: '100%',
+  margin: 'auto',
 };
 
+class MapView extends Component {
+  state = {
+    currentLocation: {
+      lat: 0,
+      lng: 0,
+    },
+    searchedLocation: '',
+    searchedLocationCoords: {
+      lat: 0,
+      lng: 0,
+    },
+  };
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+
+          this.setState({
+            currentLocation: {
+              lat: latitude,
+              lng: longitude,
+            },
+          });
+        },
+        (error) => console.log(error)
+      );
+    } else {
+      console.log('Geolocation not supported');
+    }
+  }
+
+  handleSearch = () => {
+    const { searchedLocation } = this.state;
+    const geocoder = new window.google.maps.Geocoder();
+
+    geocoder.geocode({ address: searchedLocation }, (results, status) => {
+      if (status === 'OK') {
+        const location = results[0].geometry.location;
+
+        this.setState({
+          searchedLocationCoords: {
+            lat: location.lat(),
+            lng: location.lng(),
+          },
+        });
+      } else {
+        console.log(status);
+      }
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({ searchedLocation: event.target.value });
+  };
+
+  render() {
+    const { currentLocation, searchedLocationCoords } = this.state;
+
+    return (
+      <div>
+        <Navbar />
+        <div>
+          <input type="text" onChange={this.handleChange} />
+          <button onClick={this.handleSearch}>Search</button>
+        </div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={currentLocation}
+          center={searchedLocationCoords}
+        >
+          <Marker position={currentLocation} />
+          <Marker position={searchedLocationCoords} />
+        </Map>
+      </div>
+    );
+  }
+}
+
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_API_KEY
+  apiKey: process.env.REACT_APP_API_KEY,
 })(MapView);
