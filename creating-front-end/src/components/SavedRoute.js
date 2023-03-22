258b51319b24e97f51b4c81616e66528131b72c9
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-03-22 19:14:10 -0400

    title and organization of view routes

--- creating-front-end/src/components/SavedRoute.js
+++ creating-front-end/src/components/SavedRoute.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { db, auth } from '../firebase/Firebase';
-import { ref, onValue, onChildChanged} from "firebase/database";
-import Accordion from 'react-bootstrap/Accordion'
-import Container from 'react-bootstrap/Container';
+import { ref, onValue, onChildChanged } from "firebase/database";
+import { Container, Row, Accordion } from 'react-bootstrap';
 import NavBar from './Navbar';
 import { getAuth, onAuthStateChanged } from '@firebase/auth';
 
@@ -13,12 +12,12 @@ const SavedRoute = () => {
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
-			if (user) { 
-				setUserId(user.uid);  
-				console.log("user id is: " + user.uid); 
+			if (user) {
+				setUserId(user.uid);
+				console.log("user id is: " + user.uid);
 				const recentRouteRef = ref(db, 'users/' + userId + '/recentRoutes');
 				onValue(recentRouteRef, (snapshot) => { // listens for changes in routeRef and calls callback function
-					const data = snapshot.val(); 
+					const data = snapshot.val();
 					if (data) {
 						const routesArray = Object.entries(data).map(([key, value]) => [key, value]);
 						setRecentRoutes(routesArray);
@@ -27,49 +26,59 @@ const SavedRoute = () => {
 
 				const favoriteRouteRef = ref(db, 'users/' + userId + '/savedRoutes');
 				onValue(favoriteRouteRef, (snapshot) => { // listens for changes in routeRef and calls callback function
-					const data = snapshot.val(); 
+					const data = snapshot.val();
 					if (data) {
 						const routesArray = Object.entries(data).map(([key, value]) => [key, value]);
 						setFavRoutes(routesArray);
 					};
 				});
-			} else { 
-				console.log("user not set"); 
+			} else {
+				console.log("user not set");
 			};
 		});
 	}, [userId]); // userId dependency will not cause a loop since onAuthStateChanged() will only be called upon login/logout
 
-	return ( 
+	return (
 		<Container fluid>
-			<NavBar/>
-			<Accordion alwaysOpen>
-				{favRoutes.map(route => (
-					<Accordion.Item key={route[0]} eventKey={route[0]}>
-						<Accordion.Header>{route[1].name}</Accordion.Header> 
-						<Accordion.Body>
-							{/* this is not formatted*/}
-							{/* {route[1].address}    */}
-							{/* {route[1]}  */}
-							{JSON.stringify(route[1], null, 20)} 
-						</Accordion.Body>
-					</Accordion.Item>
-				))}
-			</Accordion>
+			<NavBar />
 
-			<Accordion alwaysOpen>
-				{recentRoutes.map(route => (
-					<Accordion.Item key={route[0]} eventKey={route[0]}>
-						<Accordion.Header>{route[1].timestamp}</Accordion.Header> 
-						<Accordion.Body>
-							{/* this is not formatted*/}
-							{/* {route[1].address}    */}
-							{/* {route[1]}  */}
-							{JSON.stringify(route[1], null, 20)} 
-						</Accordion.Body>
-					</Accordion.Item>
-				))}
-			</Accordion>
-    </Container>
+			<Container style={{padding: '1rem'}} fluid>
+			<Row>
+				<h2>Favorite Routes</h2>
+			</Row>
+
+			<Row>
+				<Accordion alwaysOpen>
+					{favRoutes.map(route => (
+						<Accordion.Item key={route[0]} eventKey={route[0]}>
+							<Accordion.Header>{route[1].name}</Accordion.Header>
+							<Accordion.Body>
+								{route[1].route}
+							</Accordion.Body>
+						</Accordion.Item>
+					))}
+				</Accordion>
+			</Row>
+
+			<Row style={{marginTop: '1rem'}}>
+				<h2>Recent Routes</h2>
+			</Row>
+			<Row>
+				<Accordion alwaysOpen>
+					{recentRoutes.map(route => (
+						<Accordion.Item key={route[0]} eventKey={route[0]}>
+							<Accordion.Header>{route[1].timestamp}</Accordion.Header>
+							<Accordion.Body>
+								{route[1].route.map(r => (
+									<p>{r}</p>
+								))}
+							</Accordion.Body>
+						</Accordion.Item>
+					))}
+				</Accordion>
+			</Row>
+			</Container>
+		</Container>
 	);
 };
 export default SavedRoute; 
