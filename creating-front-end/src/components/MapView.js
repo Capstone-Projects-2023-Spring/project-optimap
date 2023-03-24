37b0525cdbe0938941f1e536094898dafe571d3c
Ivan Dat Nguyen <97911087+DatNguyen0512@users.noreply.github.com>
2023-03-24 02:19:27 -0700

    Add Location detail

--- creating-front-end/src/components/MapView.js
+++ creating-front-end/src/components/MapView.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Navbar from './Navbar';
 
+
+<style>
+  
+</style>
 const mapStyles = {
   width: '100%',
   height: '100%',
@@ -10,6 +14,10 @@ const mapStyles = {
 
 class MapView extends Component {
   state = {
+    currentLocation: {
+      lat: 0,
+      lng: 0,
+    },
     searchedLocation: '',
     searchedLocationCoords: {
       lat: 0,
@@ -18,20 +26,18 @@ class MapView extends Component {
     activeMarker: {},
     showingInfoWindow: false,
     locationInfo: {},
+    photos: [],
+    places: [],
   };
 
   componentDidMount() {
-    this.getCurrentLocation();
-  }
-
-  getCurrentLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
 
           this.setState({
-            searchedLocationCoords: {
+            currentLocation: {
               lat: latitude,
               lng: longitude,
             },
@@ -42,35 +48,53 @@ class MapView extends Component {
     } else {
       console.log('Geolocation not supported');
     }
-  };
+  }
 
   handleSearch = () => {
     const { searchedLocation } = this.state;
     const geocoder = new window.google.maps.Geocoder();
-  
+    const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
+
     geocoder.geocode({ address: searchedLocation }, (results, status) => {
       if (status === 'OK') {
         const location = results[0].geometry.location;
-        const name = results[0].address_components[0].long_name;
-        const address = results[0].formatted_address;
-  
-        this.setState({
-          searchedLocationCoords: {
-            lat: location.lat(),
-            lng: location.lng(),
+
+        placesService.textSearch(
+          {
+            query: searchedLocation,
+            location,
+            radius: '500',
           },
-          showingInfoWindow: false,
-          locationInfo: {
-            name: name,
-            address: address,
-          },
-        });
+          (places, status) => {
+            if (status === 'OK') {
+              this.setState({
+                searchedLocationCoords: {
+                  lat: location.lat(),
+                  lng: location.lng(),
+                },
+                showingInfoWindow: false,
+                locationInfo: places[0],
+                places,
+              });
+
+              if (places[0].photos) {
+                places[0].photos.forEach((photo) => {
+                  const url = photo.getUrl({ maxWidth: 500, maxHeight: 500 });
+                  this.setState((prevState) => ({
+                    photos: [...prevState.photos, url],
+                  }));
+                });
+              }
+            } else {
+              console.log(status);
+            }
+          }
+        );
       } else {
         console.log(status);
       }
     });
   };
-  
 
   handleChange = (event) => {
     this.setState({ searchedLocation: event.target.value });
@@ -81,7 +105,32 @@ class MapView extends Component {
       activeMarker: marker,
       showingInfoWindow: true,
       locationInfo: props.locationInfo,
+      photos: [],
     });
+
+    const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
+
+    placesService.getDetails(
+      { placeId: props.locationInfo.place_id },
+      (place, status) => {
+        if (status === 'OK') {
+          this.setState({
+            locationInfo: place,
+          });
+
+          if (place.photos) {
+            place.photos.forEach((photo) => {
+              const url = photo.getUrl({ maxWidth: 500, maxHeight: 500 });
+              this.setState((prevState) => ({
+                photos: [...prevState.photos, url],
+              }));
+            });
+          }
+        } else {
+          console.log(status);
+        }
+      }
+    );
   };
 
   render() {
@@ -91,8 +140,10 @@ class MapView extends Component {
       activeMarker,
       showingInfoWindow,
       locationInfo,
+      photos,
+      places,
     } = this.state;
-  
+
     return (
       <div>
         <Navbar />
@@ -109,23 +160,36 @@ class MapView extends Component {
         >
           {searchedLocationCoords.lat !== 0 && (
             <Marker
+              onClick={this.handleMarkerClick}
               position={searchedLocationCoords}
               locationInfo={locationInfo}
-              onClick={this.handleMarkerClick}
             />
           )}
-          <InfoWindow marker={activeMarker} visible={showingInfoWindow}>
+
+          <InfoWindow
+            marker={activeMarker}
+            visible={showingInfoWindow}
+          >
             <div>
-              <h2>{locationInfo.name}</h2>
-              <p>{locationInfo.address}</p>
+              <div>
+                <img src={photos[0]} alt="photo_0" />
+              </div>
+              <div>{locationInfo.formatted_phone_number}</div>
+              <div>{locationInfo.rating} / 5.0</div>
+              <div>
+                {places.map((place, index) => (
+                  <div key={index}>
+                    {place.name} - {place.formatted_address}
+                  </div>
+                ))}
+              </div>
             </div>
           </InfoWindow>
         </Map>
       </div>
     );
   }
-} 
-
+}
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_API_KEY,
 })(MapView);
