047252e5a985dc40a2ef51e8c0af5463e5ee4383
Ivan Dat Nguyen <97911087+DatNguyen0512@users.noreply.github.com>
2023-03-24 01:01:19 -0700

    Add simple infoWindow to show address of searched location

--- creating-front-end/src/components/MapView.js
+++ creating-front-end/src/components/MapView.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Navbar from './Navbar';
 
-const mapStyles = {////
+const mapStyles = {
   width: '100%',
   height: '100%',
   margin: 'auto',
@@ -10,25 +10,28 @@ const mapStyles = {////
 
 class MapView extends Component {
   state = {
-    currentLocation: {
-      lat: 0,
-      lng: 0,
-    },
     searchedLocation: '',
     searchedLocationCoords: {
       lat: 0,
       lng: 0,
     },
+    activeMarker: {},
+    showingInfoWindow: false,
+    locationInfo: {},
   };
 
   componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+  getCurrentLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
 
           this.setState({
-            currentLocation: {
+            searchedLocationCoords: {
               lat: latitude,
               lng: longitude,
             },
@@ -39,20 +42,27 @@ class MapView extends Component {
     } else {
       console.log('Geolocation not supported');
     }
-  }
+  };
 
   handleSearch = () => {
     const { searchedLocation } = this.state;
     const geocoder = new window.google.maps.Geocoder();
-
+  
     geocoder.geocode({ address: searchedLocation }, (results, status) => {
       if (status === 'OK') {
         const location = results[0].geometry.location;
-
+        const name = results[0].address_components[0].long_name;
+        const address = results[0].formatted_address;
+  
         this.setState({
           searchedLocationCoords: {
             lat: location.lat(),
             lng: location.lng(),
+          },
+          showingInfoWindow: false,
+          locationInfo: {
+            name: name,
+            address: address,
           },
         });
       } else {
@@ -60,14 +70,29 @@ class MapView extends Component {
       }
     });
   };
+  
 
   handleChange = (event) => {
     this.setState({ searchedLocation: event.target.value });
   };
 
-  render() {
-    const { currentLocation, searchedLocationCoords } = this.state;
+  handleMarkerClick = (props, marker, e) => {
+    this.setState({
+      activeMarker: marker,
+      showingInfoWindow: true,
+      locationInfo: props.locationInfo,
+    });
+  };
 
+  render() {
+    const {
+      currentLocation,
+      searchedLocationCoords,
+      activeMarker,
+      showingInfoWindow,
+      locationInfo,
+    } = this.state;
+  
     return (
       <div>
         <Navbar />
@@ -82,14 +107,26 @@ class MapView extends Component {
           initialCenter={currentLocation}
           center={searchedLocationCoords}
         >
-          <Marker position={currentLocation} />
-          <Marker position={searchedLocationCoords} />
+          {searchedLocationCoords.lat !== 0 && (
+            <Marker
+              position={searchedLocationCoords}
+              locationInfo={locationInfo}
+              onClick={this.handleMarkerClick}
+            />
+          )}
+          <InfoWindow marker={activeMarker} visible={showingInfoWindow}>
+            <div>
+              <h2>{locationInfo.name}</h2>
+              <p>{locationInfo.address}</p>
+            </div>
+          </InfoWindow>
         </Map>
       </div>
     );
   }
-}
+} 
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_API_KEY,
 })(MapView);
+
