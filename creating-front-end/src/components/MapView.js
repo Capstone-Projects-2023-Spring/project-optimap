a2fdd5e57f070a6165f0a1fb58ab53007645b8e9
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-03-28 21:54:20 -0400

    zooms on users current loc

--- creating-front-end/src/components/MapView.js
+++ creating-front-end/src/components/MapView.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow, Polyline } from 'google-maps-react';
 import Navbar from './Navbar';
 import greenMarker from '../assets/green-dot.png';
 import flag from '../assets/beachflag.png';
+import { useLocation } from "react-router-dom";
+
 
 const mapStyles = {
   width: '100%',
@@ -10,51 +12,55 @@ const mapStyles = {
   margin: 'auto',
 };
 
-class MapView extends Component {
-  state = {
-    currentLocation: {
-      lat: 0,
-      lng: 0,
-    },
-    searchedLocation: '',
-    searchedLocationCoords: {
-      lat: 0,
-      lng: 0,
-    },
-    markers: [],
-    showRoute: false,
-    directions: null,
-    error: null,
-    destinationInput: '',
-    activeMarker: {},
-    showingInfoWindow: false,
-    locationInfo: {},
-    photos: [],
-    places: [],
-  };
+const MapView = () => {
 
-  componentDidMount() {
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [searchedLocation, setSearchedLocation] = useState('');
+  const [searchedLocationCoords, setSearchedLocationCoords] = useState({
+    lat: 0,
+    lng: 0,
+  });
+  const [markers, setMarkers] = useState([]);
+  const [showRoute, setShowRoute] = useState(false);
+  const [directions, setDirections] = useState(null);
+  const [error, setError] = useState(null);
+  const [destinationInput, setDestinationInput] = useState('');
+  const [activeMarker, setActiveMarker] = useState({});
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const [locationInfo, setLocationInfo] = useState({});
+  const [photos, setPhotos] = useState([]);
+  const [places, setPlaces] = useState([]);
+
+
+
+  useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-
-          this.setState({
-            currentLocation: {
-              lat: latitude,
-              lng: longitude,
-            },
+          setCurrentLocation({
+            lat: latitude,
+            lng: longitude
           });
+
         },
         (error) => console.log(error)
       );
+
+
+
     } else {
       console.log('Geolocation not supported');
+      setCurrentLocation({
+        lat: 0,
+        lng: 0
+      })
     }
+  }, []);
 
-    //Creates autocomplete suggestions for the destination input field
+  useEffect(() => {
     const input = document.getElementById('destination-input');
-    const autocomplete = new this.props.google.maps.places.Autocomplete(input);
+    const autocomplete = new window.google.maps.places.Autocomplete(input);
     autocomplete.addListener('place_changed', () => {
       const place = autocomplete.getPlace();
       if (place.geometry) {
@@ -62,18 +68,19 @@ class MapView extends Component {
           lat: place.geometry.location.lat(),
           lng: place.geometry.location.lng(),
         };
-        const newMarkers = [...this.state.markers, { position: location }];
-        this.setState({ markers: newMarkers, destinationInput: '' });
+        const newMarkers = [...markers, { position: location }];
+        setMarkers(newMarkers);
+        setDestinationInput('');
       } else {
-        this.setState({ error: 'Failed to geocode destination.' });
+        setError('Failed to geocode destination.');
       }
     });
 
-    this.setState({ autocomplete });
-  }
+    // autocompleteRef.current = autocomplete; ?
+  }, [markers, window.google.maps.places.Autocomplete]);
 
-  handleSearch = () => {
-    const { searchedLocation } = this.state;
+
+  const handleSearch = () => {
     const geocoder = new window.google.maps.Geocoder();
     const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
 
@@ -89,22 +96,18 @@ class MapView extends Component {
           },
           (places, status) => {
             if (status === 'OK') {
-              this.setState({
-                searchedLocationCoords: {
-                  lat: location.lat(),
-                  lng: location.lng(),
-                },
-                showingInfoWindow: false,
-                locationInfo: places[0],
-                places,
+              setSearchedLocationCoords({
+                lat: location.lat(),
+                lng: location.lng(),
               });
+              setShowingInfoWindow(false);
+              setLocationInfo(places[0]);
+              setPlaces(places);
 
               if (places[0].photos) {
                 places[0].photos.forEach((photo) => {
                   const url = photo.getUrl({ maxWidth: 500, maxHeight: 500 });
-                  this.setState((prevState) => ({
-                    photos: [...prevState.photos, url],
-                  }));
+                  setPhotos((prevState) => [...prevState, url]);
                 });
               }
             } else {
@@ -118,149 +121,131 @@ class MapView extends Component {
     });
   };
 
-  handleDestinationChange = (event) => {
-    this.setState({ destinationInput: event.target.value });
+  const handleDestinationChange = (event) => {
+    setDestinationInput(event.target.value);
   };
 
-  //Handles the adding of destinations and markers associated with them
-  handleAddDestination = () => {
-    const { destinationInput, markers } = this.state;
-    const geocoder = new this.props.google.maps.Geocoder();
-    geocoder.geocode({ address: destinationInput }, (results, status) => {
-      if (status === 'OK') {
-        const location = results[0].geometry.location;
-        const newMarkers = [...markers, { position: location }];
-        this.setState({ markers: newMarkers, destinationInput: '' });
-      } else {
-        this.setState({ error: 'Failed to geocode destination.' });
-      }
-    });
-  };
-
-    handleMarkerClick = (props, marker, e) => {
-      this.setState({
-        activeMarker: marker,
-        showingInfoWindow: true,
-        locationInfo: props.locationInfo,
-        photos: [],
+  const handleAddDestination = () => {
+    const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address: destinationInput }, (results, status) => {
+        if (status === 'OK') {
+          const location = results[0].geometry.location;
+          const newMarkers = [...markers, { position: location }];
+          setMarkers(newMarkers);
+          setMarkers(newMarkers)
+          console.log("markers")
+          console.dir(newMarkers)
+          setDestinationInput('');
+        } else {
+          setError('Failed to geocode destination.');
+        }
       });
-  
-      const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
-  
-      placesService.getDetails(
-        { placeId: props.locationInfo.place_id },
-        (place, status) => {
-          if (status === 'OK') {
-            this.setState({
-              locationInfo: place,
-            });
-  
-            if (place.photos) {
-              place.photos.forEach((photo) => {
-                const url = photo.getUrl({ maxWidth: 500, maxHeight: 500 });
-                this.setState((prevState) => ({
-                  photos: [...prevState.photos, url],
-                }));
-              });
-            }
-          } else {
-            console.log(status);
-          }
-        }
-      );
-    };
+  };
 
-    handleChange = (event) => {
-      this.setState({ searchedLocation: event.target.value });
-    };
-  
-    //Handles destination removal (now invoked when marker is clicked)
-    handleRemoveDestination = (index) => {
-      const { markers } = this.state;
-      if (markers.length === 1) {
-        this.setState({ markers: [], showRoute: false }); // set showRoute to false if last destination is removed
-      } else {
-        const newMarkers = [...markers.slice(0, index), ...markers.slice(index + 1)];
-        this.setState({ markers: newMarkers }, () => {
-          if (newMarkers.length > 0) {
-            this.handleShowRoute();
-          } else {
-            this.setState({ showRoute: false }); // set showRoute to false if all destinations are removed
+  const handleMarkerClick = (props, marker, e) => {
+
+    setActiveMarker(marker)
+    setShowingInfoWindow(true)
+    setLocationInfo(props.locationInfo)
+    setPhotos([]);
+
+    const placesService = new window.google.maps.places.PlacesService(document.createElement('div'));
+
+    placesService.getDetails(
+      { placeId: props.locationInfo.place_id },
+      (place, status) => {
+        if (status === 'OK') {
+          setLocationInfo(place)
+
+          if (place.photos) {
+            place.photos.forEach((photo) => {
+              const url = photo.getUrl({ maxWidth: 500, maxHeight: 500 });
+                setPhotos([...photos, url])
+            });
           }
-        });
+        } else {
+          console.log(status);
+        }
       }
-    };
+    );
+  };
 
-    //Handles the display of the route
-    handleShowRoute = () => {
-      const { markers, currentLocation } = this.state;
-      const DirectionsService = new this.props.google.maps.DirectionsService();
-      DirectionsService.route(
-        {
-          origin: currentLocation,
-          destination: markers[markers.length - 1].position,
-          waypoints: markers.slice(0, markers.length - 1).map((marker) => ({ location: marker.position })),
-          optimizeWaypoints: true,
-          travelMode: this.props.google.maps.TravelMode.DRIVING,
-        },
-        (result, status) => {
-          if (status === this.props.google.maps.DirectionsStatus.OK) {
-            this.setState({
-              directions: result,
-              showRoute: true,
-            });
-          } else {
-            this.setState({ error: 'Failed to fetch directions.' });
-          }
+
+
+  const handleChange = (event) => {
+    setSearchedLocation(event.target.value);
+  };
+
+  const handleRemoveDestination = (index) => {
+    if (markers.length === 1) {
+      // setMarkers([]);
+      setShowRoute(false); // set showRoute to false if last destination is removed
+    } else {
+      const newMarkers = [...markers.slice(0, index), ...markers.slice(index + 1)];
+      setMarkers(newMarkers);
+      if (newMarkers.length > 0) {
+        handleShowRoute();
+      } else {
+        setShowRoute(false); // set showRoute to false if all destinations are removed
+      }
+    }
+  };
+
+
+  const handleShowRoute = (markers, currentLocation, google) => {
+
+    const DirectionsService = new window.google.maps.DirectionsService();
+    DirectionsService.route(
+      {
+        origin: currentLocation,
+        destination: markers[markers.length - 1].position,
+        waypoints: markers.slice(0, markers.length - 1).map((marker) => ({ location: marker.position })),
+        optimizeWaypoints: true,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          setDirections(result)
+          setShowRoute(true);
+        } else {
+          setError('Failed to fetch directions.');
+          console.log("no directions")
         }
-      );
-    };
-    
-    render() {
-    const {
-    currentLocation,
-    searchedLocationCoords,
-    markers,
-    showRoute,
-    directions,
-    destinationInput,
-    activeMarker,
-    showingInfoWindow,
-    locationInfo,
-    photos,
-    places,
-    } = this.state;
+      }
+    );
+  };
 
-    return (
 
-      <div>
-        <Navbar />
-        <div className="map-container">
-          <div className="search-container">
-            <input id="search" type="text" onChange={this.handleChange} />
-            <button onClick={this.handleSearch}>Search</button>
-            <div className="input-container">
-              <input
-                id="destination-input"
-                type="text"
-                placeholder="Enter a destination"
-                value={destinationInput}
-                onChange={this.handleDestinationChange}
-              />
-              <button onClick={this.handleAddDestination}>Add</button>
-              <button onClick={this.handleShowRoute}>Show Route</button>
-            </div>
+  return (
+    <div>
+    <Navbar />
+    {currentLocation ? (
+      <div className="map-container">
+        <div className="search-container">
+          <input id="search" type="text" onChange={handleChange} />
+          <button onClick={handleSearch}>Search</button>
+          <div className="input-container">
+            <input
+              id="destination-input"
+              type="text"
+              placeholder="Enter a destination"
+              value={destinationInput}
+              onChange={handleDestinationChange}
+            />
+            <button onClick={handleAddDestination}>Add</button>
+            <button onClick={handleShowRoute}>Show Route</button>
           </div>
-          <Map
-          google={this.props.google}
+        </div>
+        <Map
+          google={window.google}
           zoom={14}
           style={mapStyles}
           initialCenter={currentLocation}
-          center={searchedLocationCoords}
+          // center={searchedLocationCoords}
         >
           {searchedLocationCoords.lat !== 0 && (
             <Marker
-              onClick={this.handleMarkerClick}
+              onClick={handleMarkerClick}
               position={searchedLocationCoords}
               locationInfo={locationInfo}
             />
@@ -268,19 +253,19 @@ class MapView extends Component {
           {currentLocation.lat && currentLocation.lng && (
             <Marker
               position={{ lat: currentLocation.lat, lng: currentLocation.lng }}
-              icon={flag}/>
+              icon={flag} />
           )}
           {markers.map((marker, index) => (
-            <Marker key={index} position={marker.position} onClick={() => this.handleRemoveDestination(index)} icon={greenMarker} />
+            <Marker key={index} position={marker.position} onClick={() => handleRemoveDestination(index)} icon={greenMarker} />
           ))}
-          
+
           {showRoute && directions && (
-          <Polyline
-            path={directions.routes[0].overview_path}
-            strokeColor="#00d4ff"
-            strokeOpacity={0.8}
-            strokeWeight={4}
-          />
+            <Polyline
+              path={directions.routes[0].overview_path}
+              strokeColor="#00d4ff"
+              strokeOpacity={0.8}
+              strokeWeight={4}
+            />
           )}
           <InfoWindow
             marker={activeMarker}
@@ -302,11 +287,15 @@ class MapView extends Component {
             </div>
           </InfoWindow>
         </Map>
-        </div>
       </div>
-    );
-    }
-    }
+    ) : (
+
+      <div>Loading...</div>
+    )}
+        </div>
+  );
+}
+
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_API_KEY,
 })(MapView);
