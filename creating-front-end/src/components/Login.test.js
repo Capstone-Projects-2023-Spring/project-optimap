111ed446938fbcbf3c4349c5162d9d568f6418ea
AlexHarvey63 <89492718+AlexHarvey63@users.noreply.github.com>
2023-04-22 20:36:09 +0000

    Added more test cases

--- creating-front-end/src/components/Login.test.js
+++ creating-front-end/src/components/Login.test.js
@@ -11,6 +11,7 @@ jest.mock('react-router-dom', () => ({
 }));
 
 describe('Login', () => {
+// Test If Login Form Renders
   it('should render the Login form', () => {
     render(<MemoryRouter><Login /></MemoryRouter>);
     const emailInput = screen.getByLabelText('Email address');
@@ -21,6 +22,73 @@ describe('Login', () => {
     expect(loginButton).toBeInTheDocument();
   });
 
+// Test If Email Field Renders
+it('should display Email Field', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const emailInput = screen.getByLabelText('Email address');
+    expect(emailInput).toBeInTheDocument();
+    });
+
+// Test If Password Field Renders
+it('should display Password Field', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const passwordInput = screen.getByLabelText('Password');
+    expect(passwordInput).toBeInTheDocument();
+    });
+
+// Test if Login Button Renders
+it('should display Login Button', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const loginButton = screen.getByTestId('login button');
+    expect(loginButton).toBeInTheDocument();
+    });
+
+// Verify Email handler function responds to a change of event in the email field
+it('should respond to a change of event in the email field', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const emailInput = screen.getByLabelText('Email address');
+    fireEvent.change(emailInput, { target: { value: ' ' } });
+    expect(emailInput.value).toBe('');
+    });
+
+// Verify Password handler function responds to a change of event in the password field
+it('should respond to a change of event in the password field', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const passwordInput = screen.getByLabelText('Password');
+    fireEvent.change(passwordInput, { target: { value: ' ' } });
+    expect(passwordInput.value).toBe(' ');
+    });
+
+// Verify handler function responds to a change of event on form submission
+it('should respond to a change of event on form submission', () => {
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const loginButton = screen.getByTestId('login button');
+    fireEvent.click(loginButton);
+    expect(loginButton).toBeInTheDocument();
+    });
+// Verify if null entry displays an error
+it('should display an error message if login fails with null entry', async () => {
+    const mockSignInWithEmailAndPassword = jest.fn(() => {
+        throw new Error('auth/invalid-email');
+    });
+    jest.mock('../firebase/Firebase', () => ({
+        signInWithEmailAndPassword: mockSignInWithEmailAndPassword,
+    }));
+
+    render(<MemoryRouter><Login /></MemoryRouter>);
+    const emailInput = screen.getByLabelText('Email address');
+    const passwordInput = screen.getByLabelText('Password');
+    const loginButton = screen.getByTestId('login button');
+
+    fireEvent.change(emailInput, { target: { value: ' ' } });
+    fireEvent.change(passwordInput, { target: { value: ' ' } });
+    fireEvent.click(loginButton);
+
+    const errorMessage = await screen.findByText('Firebase: Error (auth/invalid-email).');
+    expect(errorMessage).toBeInTheDocument();
+    });
+
+// Verify if invalid password displays an error
   it('should display an error message if login fails with password', async () => {
     const mockSignInWithEmailAndPassword = jest.fn(() => {
       throw new Error('auth/wrong-password');
@@ -34,14 +102,15 @@ describe('Login', () => {
     const passwordInput = screen.getByLabelText('Password');
     const loginButton = screen.getByTestId('login button');
 
-    fireEvent.change(emailInput, { target: { value: 'a@a.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'te1vwsedv23' } }); // wrong password
+    fireEvent.change(emailInput, { target: { value: 'b@b.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'te1vwsed23' } }); // wrong password
     fireEvent.click(loginButton);
 
     const errorMessage = await screen.findByText('Incorrect password. Please try again.');
     expect(errorMessage).toBeInTheDocument();
   });
 
+// Verify if invalid email displays an error
   it('should display an error message if login fails with email', async () => {
     const mockSignInWithEmailAndPassword = jest.fn(() => {
       throw new Error('auth/wrong-email');
@@ -55,7 +124,7 @@ describe('Login', () => {
     const passwordInput = screen.getByLabelText('Password');
     const loginButton = screen.getByTestId('login button');
 
-    fireEvent.change(emailInput, { target: { value: 'test@twefwefwest.com' } }); // Wrong email
+    fireEvent.change(emailInput, { target: { value: 'test@twefwewst.com' } }); // Wrong email
     fireEvent.change(passwordInput, { target: { value: 'test123' } });
     fireEvent.click(loginButton);
 
@@ -63,6 +132,7 @@ describe('Login', () => {
     expect(errorMessage).toBeInTheDocument();
   });
 
+// Verify if successful login displays navbar options
   it('should display navbar options after successful login', async () => {
     const mockUser = {
       uid: '1234',
